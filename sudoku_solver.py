f71991ebf210b2d6cd24da7ed884d1d12f7c8c7f
Cherub2K <48897247>
2019-12-27 14:59:45 +1300

    initial commit

---
+++ sudoku_solver.py
@@ -0,0 +1,85 @@
+def solve(board):
+    """ Attempts to place candidates 1-9 in unprocessed squares by
+    recursively backtracking to fill the board and returning True once there
+    no more unprocessed squares hence the Sudoku has been solved.
+    """
+    square = find_unprocessed_square(board)
+    if not square:  # Terminates if there are no more unprocessed squares
+        # Prints completed board
+        print(print_board(board))
+        return True
+    else:
+        row, col = square
+    for candidate in range(1, 10, 1):
+        if should_prune(board, candidate, (row, col)) is False:
+            board[row][col] = candidate
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+
+def should_prune(board, candidate, pos):
+    """ Takes a candidate and returns true if it can be determined that they
+    cannot lead to a solution.
+    """
+    row, col = pos[0], pos[1]  # pos is the tuple from find_empty_square
+    # Check row
+    for i in range(9):
+        if board[row][i] == candidate and col != i:
+            return True
+    # Check column
+    for i in range(9):
+        if board[i][col] == candidate and row != i:
+            return True
+    # Check square
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == candidate and (i, j) != pos:
+                return True
+    return False
+
+
+def find_unprocessed_square(board):
+    """ Finds an unprocessed square on the board and returns the position as
+    a tuple representing its row and column
+    """
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:  # 0 is denoted for unprocessed squares
+                return i, j
+    return None
+
+
+def print_board(board):
+    """ Returns string representation of board.
+    """
+    board_str = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if j == 3 or j == 6:
+                board_str.append("| ")
+            board_str.append(str(board[i][j]))
+            board_str.append(" ")
+        board_str.append("\n")
+        if i == 2 or i == 5:
+            board_str.append("- - - - - - - - - - -")
+            board_str.append("\n")
+    return "".join(board_str)
+
+
+solve(board=[
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+])
