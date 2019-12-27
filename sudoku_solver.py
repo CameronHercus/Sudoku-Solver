4f1bb9597f4d393addac6595ad029ab82a441a6f
Cherub2K <48897247>
2019-12-28 10:30:43 +1300

    minor changes

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,8 +5,6 @@ def solve(board):
     """
     square = find_unprocessed_square(board)
     if not square:  # Terminates if there are no more unprocessed squares
-        # Prints completed board
-        print(print_board(board))
         return True
     else:
         row, col = square
@@ -32,9 +30,9 @@ def should_prune(board, candidate, pos):
     for i in range(9):
         if board[i][col] == candidate and row != i:
             return True
-    # Check square
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+    # Check box
+    box_x = col // 3
+    box_y = row // 3
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
             if board[i][j] == candidate and (i, j) != pos:
@@ -70,7 +68,7 @@ def print_board(board):
     return "".join(board_str)
 
 
-solve(board=[
+sudoku_board = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
     [6, 0, 0, 0, 7, 5, 0, 0, 9],
     [0, 0, 0, 6, 0, 1, 0, 7, 8],
@@ -82,4 +80,9 @@ solve(board=[
     [0, 7, 0, 3, 0, 0, 0, 1, 2],
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7],
-])
+]
+print("Initial board")
+print(print_board(sudoku_board))
+solve(sudoku_board)
+print("Solved board")
+print(print_board(sudoku_board))
